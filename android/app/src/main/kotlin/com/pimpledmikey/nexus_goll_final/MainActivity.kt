3d75d24b5303e484912ea2567d91dcdfa4d72afa
Miguel Angel Avila Requena <pimpledmikey@gmail.com>
2026-01-15 09:33:44 -0600

    chore: add MainActivity (Flutter embedding v2)

--- android/app/src/main/kotlin/com/pimpledmikey/nexus_goll_final/MainActivity.kt
+++ android/app/src/main/kotlin/com/pimpledmikey/nexus_goll_final/MainActivity.kt
@@ -1,21 +1,7 @@
 package com.pimpledmikey.nexus_goll_final
 
-import android.os.Bundle
-import androidx.core.view.WindowCompat
-import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.android.FlutterActivity
 
-/**
- * MainActivity con soporte Edge-to-Edge para Android 15+
- * 
- * - WindowCompat.setDecorFitsSystemWindows(window, false) habilita el modo edge-to-edge
- * - Esto permite que la app se dibuje detrás de las barras de sistema
- * - Flutter maneja los insets automáticamente con SafeArea/MediaQuery
- */
-class MainActivity : FlutterFragmentActivity() {
-    
-    override fun onCreate(savedInstanceState: Bundle?) {
-        // Habilitar edge-to-edge ANTES de llamar a super.onCreate()
-        WindowCompat.setDecorFitsSystemWindows(window, false)
-        super.onCreate(savedInstanceState)
-    }
+// MainActivity actualizado al Android embedding V2 requerido por Flutter
+class MainActivity: FlutterActivity() {
 }
